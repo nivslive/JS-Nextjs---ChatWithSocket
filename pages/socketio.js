c6dc5f884472ceca2af9fa9ee7c11dd9414dc628
nivslive <nivsoficial@gmail.com>
2022-11-17 20:30:40 -0300

    fixes on build

--- pages/socketio.js
+++ pages/socketio.js
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 // import { useState } from "react";
 
 const URL = "http://localhost:3001";
-const socket = io(URL, { transports: ["websocket"] });
+const Socket = io(URL, { transports: ["websocket"] });
 // socket.onAny((event, ...args) => {
 //   console.log(event, args);
 // });
-export default socket;
+export default Socket;
