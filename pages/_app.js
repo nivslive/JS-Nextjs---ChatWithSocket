4625d8b2f6272c4011e2af3134e8ea88714a8fe2
nivslive <nivsoficial@gmail.com>
2022-11-27 10:29:06 -0300

    atts error runtime

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/Home.module.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../styles/style.css";
+import 'bootstrap/dist/css/bootstrap.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
